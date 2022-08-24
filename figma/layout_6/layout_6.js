5dcb0dfc216a83366be0a80bf29b12843b3b93a7
forvedtari <94034713+forvedtari@users.noreply.github.com>
2022-08-24 16:49:51 +0530

    Add files via upload

---
+++ figma/layout_6/layout_6.js
@@ -0,0 +1,8 @@
+function myfunction(){
+    if(document.getElementById('sidebar').style.display=='none'){
+        document.getElementById('sidebar').style.display='block';
+    }
+    else{
+        document.getElementById('sidebar').style.display='none';
+    }
+}
---
+++ figma/layout_6mod/mod.js
@@ -0,0 +1,8 @@
+function myfunction(){
+    if(document.getElementById('menu').style.display=='none'){
+        document.getElementById('menu').style.display='block';
+    }
+    else{
+        document.getElementById('menu').style.display='none';
+    }
+}
